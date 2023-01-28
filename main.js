2303811f36ffc5baac2758af79320610784b72bd
bibisixtynine <85257463+bibisixtynine@users.noreply.github.com>
2023-01-28 16:22:25 +0100

    2 files modified, 1 added, 4 deleted

--- main.js
+++ main.js
@@ -5,21 +5,30 @@ const appDiv = document.getElementById('app');
 
 let prevent = false;
 
-document.body.addEventListener('beforecopy', (e) => {
-  console.log('before');
+document.addEventListener('beforecopy', (e) => {
+  console.log('#event: beforecopy');
   if (prevent) e.preventDefault();
 });
 
-document.body.addEventListener('beforecut', (e) => {
-  console.log('beforecut');
+document.addEventListener('beforecut', (e) => {
+  console.log('#event: beforecut');
   if (prevent) e.preventDefault();
 });
+
+function setPreventIndicator(state) {
+  let light = document.getElementById('light');
+  if (state) {
+    light.style = 'color:red';
+    light.innerHTML = 'preventDefault enabled'
+  } else { 
+    light.style = 'color:green';
+    light.innerHTML = 'preventDefault disabled'
+  }
+}
 
 const button = document.getElementById('button');
 button.addEventListener('click', (e) => {
   console.log('click');
   prevent = !prevent;
-  let light = document.getElementById('light');
-  if (prevent) light.style = 'color:red';
-  else light.style = 'color:green';
+  setPreventIndicator(prevent)
 });
